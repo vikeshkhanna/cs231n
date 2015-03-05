e26a4a8594f3f33412cbe4e8395020f47bcd0c31
Vikesh Khanna <khanna.vikesh@gmail.com>
2015-03-04 21:45:54 -0800

    Adding classifier

---
+++ funcs/classifier.py
@@ -0,0 +1,33 @@
+import numpy as np
+import caffe
+
+def transform_label (caffe_label):
+    if caffe_label >=0 and caffe_label <=9:
+        return caffe_label
+    elif caffe_label >=10 and caffe_label <= 35:
+        return chr(ord('A') + caffe_label - 10)
+    elif caffe_label >=36 and caffe_label <= 61:
+        return chr(ord('a') + caffe_label - 36)
+    raise "Invalid label from Caffe"
+
+class Classifier:
+  def __init__(self, model_file, pretrained_file, gpu=False):
+    self.MODEL_FILE = model_file
+    self.PRETRAINED = pretrained_file
+    
+    if gpu:
+      caffe.set_mode_gpu()
+    else:
+      caffe.set_mode_cpu()
+
+    self.classifier = caffe.Classifier(self.MODEL_FILE, self.PRETRAINED)
+
+class Recognizer(Classifier):
+    # Recognizes a list of images
+  def recognize(self, np_images):
+    return self.classifier.predict(np_images)
+
+class Detector(Classifier):
+  # Recognizes a list of images
+  def detect(self, np_images):
+    return self.classifier.predict(np_images)
